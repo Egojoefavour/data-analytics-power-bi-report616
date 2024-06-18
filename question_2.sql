a845146065c72b0f1ac8a01d0f2300d40be04698
Egojoefavour <145851841+Egojoefavour@users.noreply.github.com>
2024-06-18 03:45:23 +0100

    remove file

--- question_2.sql
+++
@@ -1,8 +0,0 @@
---Which month in 2022 has had the highest revenue?
-
-SELECT month_name, sum(revenue) AS highest_revenue 
-FROM data_analytics_view
-WHERE year = 2022
-GROUP BY month_name
-ORDER BY sum(revenue) DESC
-LIMIT 1
